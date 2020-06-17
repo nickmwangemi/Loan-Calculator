148904092a707b0e43c83f6451a446326a04f108
Nick Mwangemi <nickmwangemi@gmail.com>
2020-06-17 13:15:25 +0300

    Added error handling and iutputting of the error message to the user

--- app.js
+++ app.js
@@ -16,7 +16,8 @@ function calculateResults(e) {
     const principal = parseFloat(amount.value)
     const calculatedInterest = parseFloat(interest.value) / 100 / 12
     const calculatedPayments = parseFloat(years.value) * 12
-        // compute monthly payments
+
+    // compute monthly payments
     const x = Math.pow(1 + calculatedInterest, calculatedPayments)
     const monthly = (principal * x * calculatedInterest) / (x - 1)
 
@@ -25,10 +26,35 @@ function calculateResults(e) {
         monthlyPayment.value = monthly.toFixed(2)
         totalPayment.value = (monthly * calculatedPayments).toFixed(2)
         totalInterest.value = ((monthly * calculatedPayments) - principal).toFixed(2)
-    } else {}
-
-
-
-
+    } else {
+        showError('Please check your values.')
+    }
     e.preventDefault()
+}
+
+// show error
+function showError(error) {
+    // create a div
+    const errorDiv = document.createElement('div')
+
+    // get elements
+    const card = document.querySelector('.card')
+    const heading = document.querySelector('.heading')
+
+    // add class
+    errorDiv.className = 'alert alert-danger'
+
+    // create text node and append to div
+    errorDiv.appendChild(document.createTextNode(error))
+
+    // insert error above heading
+    card.insertBefore(errorDiv, heading)
+
+    // clear error after 3 seconds
+    setTimeout(clearError, 3000)
+}
+
+// clear error
+function clearError() {
+    document.querySelector('.alert').remove()
 }
